2cbeb09d4c36ec4fc7e1ae3bce82f49148838596
Lekha Sri <lekhanidadavole939@gmail.com>
2020-11-22 13:05:33 +0530

    Day-8

---
+++ Day-8/src/ObjectOutputStreamEx.java
@@ -0,0 +1,9 @@
+
+public class ObjectOutputStreamEx 
+{
+    public static void main(String[] args)
+    {
+    	Student s = new Student(123,"lekha");
+    
+    }
+}
---
+++ Day-8/src/Student.java
@@ -0,0 +1,16 @@
+
+public class Student 
+{
+	public Student(int sid, String name) {
+		super();
+		this.sid = sid;
+		this.name = name;
+	}
+	int sid;
+	   String name;
+   public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
